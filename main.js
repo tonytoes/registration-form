66ec86ddbbcf1cade93ad28ad9efc59e243c8876
tonytoes <159160091+tonytoes@users.noreply.github.com>
2026-02-14 23:31:36 +0800

    chore: minor changes

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ form.addEventListener("submit", (e) => {
 
 function checkInputs() {
   // trim to remove the whitespaces
-
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const password2Value = password2.value.trim();
