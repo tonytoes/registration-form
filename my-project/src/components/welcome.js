2986f62bb32bc093df98af24fc2e30fa8b0fca5f
tonytoes <159160091+tonytoes@users.noreply.github.com>
2026-02-22 13:55:53 +0800

    feat: welcome page

---
+++ my-project/src/components/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div className='container'>
+            <h1>Welcome!</h1>
+            <p>You have successfully registered.</p>
+        </div>
+    );
+}
+
+export default Welcome;
